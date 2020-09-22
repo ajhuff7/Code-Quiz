2f3d967b31b708d4e388cc5da94a5e20fc3f9a8b
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-21 21:40:24 -0700

    answer key

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 
 var timerEl1 = document.getElementById("countdown");
 var timerEl2 = document.getElementById("clock");
+var boxes = document.getElementById("boxes");
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var option4 = document.getElementById("option4");
 var timeLeft = 90
 var scoreCard = 0
-
+var answer = ["a", "c", "d", "d", "b", "a", "d", "b", "b", "c"];
 function finalScore();{
 
 
@@ -12,7 +17,47 @@ function finalScore();{
 
 function playGame(); {
     
-    
+    option1.addEventListener("click", function() {
+        if (answers[0] == "a" || ){
+        count++;
+        option1.setAttribute("correct");
+        }
+        else (timeLeft - 5);{  
+        option1.setAttribute("wrong");
+        }
+      });
+
+      option2.addEventListener("click", function() {
+        if (answers[0] == "a"){
+        count++;
+        option2.setAttribute("correct");
+        }
+        else (timeLeft - 5);{  
+        option2.setAttribute("wrong");
+        }
+      });
+
+      option3.addEventListener("click", function() {
+        if (answers[0] == "a"){
+        count++;
+        option3.setAttribute("correct");
+        }
+        else (timeLeft - 5);{  
+        option3.setAttribute("wrong");
+        }
+      });
+
+      option4.addEventListener("click", function() {
+        if (answers[0] == "a"){
+        count++;
+        option4.setAttribute("correct");
+        }
+        else (timeLeft - 5);{  
+        option4.setAttribute("wrong");
+        }
+      });
+
+
 }
 
 function gameClock();{
