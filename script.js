2da436c2d0f5f55c385ecb61cbb4621e0f0b4609
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-22 22:46:19 -0700

    new html base

--- script.js
+++ script.js
@@ -19,11 +19,16 @@ var playersChoice = [
 
 // function finalScore();{
 
+
+  // localStorage.setItem("count", count);
     
 // }
 
 function playGame(){
     
+
+
+
     boxes.addEventListener("click", function(event) {
         for (var i = 0; i < playerAnswers.length; i++);{
         playerAnswers.push("playersChoice")
@@ -63,3 +68,49 @@ function readySet(){
   }
 
   readySet();
+
+
+
+  function renderTodos() {
+    // Clear todoList element and update todoCountSpan
+    todoList.innerHTML = "";
+    todoCountSpan.textContent = todos.length;
+  
+    // Render a new li for each todo
+    for (var i = 0; i < todos.length; i++) {
+      var todo = todos[i];
+  
+      var li = document.createElement("li");
+      li.textContent = todo;
+      todoList.appendChild(li);
+
+
+
+
+      
+// user clicks start game
+// question & answer buttons are loaded on the page
+// timer starts
+// user clicks on a button
+// check the answer to see if it's correct or incorrect
+// do some work based on the outcome of #5
+// check if there's another question to load
+// go back to step 2 if there are more questions
+// no more questions? do the end game sequence
+// load input form for user to enter name
+// user submits name
+// save user name and score to localstorage
+
+
+i'll have the question stored somewhere in code using an array, or object, or many variables
+i'll associate the answers to that question by linking array indexes, or object key/value pairs, or MANY MANY variables
+i'll write a function that does this work
+in this function i'll include something to track the current question so i can use this function again for upcoming questions
+
+create an event listener for the buttons
+in it, i'll compare the selected answer to the correct answer for the question
+determine some course of action depending on the answer
+change the current question to the next
+run the function i made for rendering questions/answers onto the page
+etc
+
