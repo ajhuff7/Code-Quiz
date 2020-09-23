51b0c08b6626a74f3c0c428f96fa4d6d990a2c85
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-23 03:02:47 -0700

    Q/A arrays

--- script.js
+++ script.js
@@ -1,116 +1,191 @@
 
-
+var starter = document.getElementById("starter");
 var timerEl = document.getElementById("clock");
-var boxes = document.getElementById("boxes");
+var questions = document.getElementById("questionsCard");
+var score = document.getElementById("scoreCard");
 var option1 = document.getElementById("option1");
 var option2 = document.getElementById("option2");
 var option3 = document.getElementById("option3");
 var option4 = document.getElementById("option4");
 var timeLeft = 90
-var scoreCard = 0
-var answerKey = ["a", "c", "d", "d", "b", "a", "d", "b", "b", "c"];
-var playerAnswers = [];
-var playersChoice = [
-    option1 = "a",
-    option2 = "b", 
-    option3 = "c",
-    option4 = "d"
+
+
+var questionsList = [
+  "1. Megan's real last name is...",
+  "2. She was born in...",
+  "3. Her mother was also a rapper under the name...",
+  "4. In college she studied...",
+  "5. Megan refers to her fans as...",
+  "6. Megan's alter ego is..",
+  "7. Which of these is not one of Megan's biggest influences:",
+  "8. Megan's year of birth was...",
 ];
 
-// function finalScore();{
+
+var answerList1 = [
+  "A) Pete" == true, 
+  "B) Jackson" == false,
+  "C) Thee" == false,
+  "D) Marino" == false,
+
+]
+
+var answerList2 = [
+  "A) New York" == false,
+  "B) Alabama" == false,
+  "C) Texas" == true,
+  "D) Ohio" == false,  
+
+]
+
+var answerList3 = [
+  "A) MC Lyte" == false,
+  "B) Lil Filly" == false,
+  "C) Susie P" == false,
+  "D) Holly Wood" == true,
+
+]
+
+var answerList4 = [
+  "A) Musical Theater" == false,
+  "B) Business and Finance" == false,
+  "C) Health Administration" == true,
+  "D) Hotel Management" == false,
+  
+]
+
+var answerList5 = [
+  "A) Thee Stable" == false,
+  "B) Hotties" == true,
+  "C) Studs" == false,
+  "D) Meganites" == false,
+  
+]
+
+var answerList6 = [
+  "A) Tina Snow" == true,
+  "B) Mercy Neaux" == false,
+  "C) Jovon Truth" == false,
+  "D) Boss Ratchet" == false,
+  
+]
+
+var answerList7 = [
+  "A) Beyonce" == false, 
+  "B) Three 6 Mafia" == false,
+  "C) Pimp C" == false,
+  "D) Queen Latifah" == true,
+  
+]
+
+var answerList8 = [
+"A) 1996" == false,
+"B) 1997" == false,
+"C) 1995" == true,
+"D) 1993" == false,
+
+]
+
+var answerGuide = [
+  answerList1, 
+  answerList2, 
+  answerList3, 
+  answerList4, 
+  answerList5, 
+  answerList6, 
+  answerList7, 
+  answerList8, 
+
+]
 
 
-  // localStorage.setItem("count", count);
+function finalScore();{
+  questionCard.textContent = timeLeft
+  localStorage.setItem("score", timeLeft);
     
-// }
-
-function playGame(){
-    
-
-
-
-    boxes.addEventListener("click", function(event) {
-        for (var i = 0; i < playerAnswers.length; i++);{
-        playerAnswers.push("playersChoice")
-        }
-
-        if (playerAnswers[0] === answerKey[0]){
-            scoreCard++;
-            event.currentTarget.setAttribute(
-                "background-color: green"
-              );
-        }
-        else (timeLeft - 5);{ 
-            event.currentTarget.setAttribute(
-                "background-color: red"
-              );
-        }
-    });
 
 }
 
-function readySet(){
-  starter.addEventListener("click", function(event) {
+function correctAnswer(){
+
+}
+
+function wrongAnswer(){
+
+}
+
+
+function playGame(){
     
-    var timeInterval = setInterval(function() {
-        timerEl.textContent = timeLeft + " seconds left...";
-        timeLeft--;
-    
-        if (timeLeft === 0) {
-          timerEl.textContent = "";
-          clearInterval(timeInterval);
-          finalScore();
+    option1.addEventListener("click", function(event) {
+        if (event.currentTarget == true);{ 
+          correctAnswer();
         }
-        
-        else playGame();
-      }, 1000);
-    });
-  }
+        elseif (event.currentTarget == false);{
+          (timeLeft - 5);
+          wrongAnswer(); 
+        }
+    })
 
-  readySet();
+    option2.addEventListener("click", function(event) {
+      if (event.currentTarget == true);{ 
+        correctAnswer();
+      }
+      elseif (event.currentTarget == false);{
+        (timeLeft - 5);
+        wrongAnswer(); 
+      }
+  })
 
+    option3.addEventListener("click", function(event) {
+      if (event.currentTarget == true);{ 
+        correctAnswer();
+      }
+      elseif (event.currentTarget == false);{
+        (timeLeft - 5);
+        wrongAnswer(); 
+      }
+  })
 
-
-  function renderTodos() {
-    // Clear todoList element and update todoCountSpan
-    todoList.innerHTML = "";
-    todoCountSpan.textContent = todos.length;
-  
-    // Render a new li for each todo
-    for (var i = 0; i < todos.length; i++) {
-      var todo = todos[i];
-  
-      var li = document.createElement("li");
-      li.textContent = todo;
-      todoList.appendChild(li);
-
-
-
-
-      
-// user clicks start game
-// question & answer buttons are loaded on the page
-// timer starts
-// user clicks on a button
-// check the answer to see if it's correct or incorrect
-// do some work based on the outcome of #5
-// check if there's another question to load
-// go back to step 2 if there are more questions
-// no more questions? do the end game sequence
-// load input form for user to enter name
-// user submits name
-// save user name and score to localstorage
+    option4.addEventListener("click", function(event) {
+      if (event.currentTarget == true);{ 
+        correctAnswer();
+      }
+      elseif (event.currentTarget == false);{
+        (timeLeft - 5);
+        wrongAnswer(); 
+      }
+  })
 
 
-i'll have the question stored somewhere in code using an array, or object, or many variables
-i'll associate the answers to that question by linking array indexes, or object key/value pairs, or MANY MANY variables
-i'll write a function that does this work
-in this function i'll include something to track the current question so i can use this function again for upcoming questions
 
-create an event listener for the buttons
-in it, i'll compare the selected answer to the correct answer for the question
-determine some course of action depending on the answer
-change the current question to the next
-run the function i made for rendering questions/answers onto the page
-etc
+function startGame() {
+  questionCard.textContent = questionList[0];
+  option1.textContent = answersGuide[0, 0];
+  option2.textContent = answersGuide[0, 1];
+  option3.textContent = answersGuide[0, 2];
+  option4.textContent = answersGuide[0, 3];
+  i++
+}
 
+
+function readySet() {
+  timeInterval = setInterval(function() {
+    timerEl.textContent = timeLeft + " seconds left...";
+    timeLeft--;
+    
+    if (timeLeft === 0) {
+      timerEl.textContent = "";
+      clearInterval(timeInterval);
+      finalScore();
+    }   
+  }, 1000);
+}
+
+starter.addEventListener("click", function(event) {
+
+startGame();
+readySet();
+playGame();
+
+})
