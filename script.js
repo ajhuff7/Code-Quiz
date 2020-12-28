bceb2cb6f8a45fbd02f76588d4b7970a48592177
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-12-28 10:45:32 -0800

    updated format

--- script.js
+++ script.js
@@ -1,144 +1,244 @@
+var start = document.getElementById("start");
+var startButton = document.getElementById("startButton");
+var questionArea = document.getElementById("questionArea");
+var question = document.getElementById("question");
+var A = document.getElementById("A");
+var B = document.getElementById("B");
+var C = document.getElementById("C");
+var D = document.getElementById("D");
+var scoreChart = document.getElementById("scoreChart");
+var score = document.getElementById("score");
+var player = document.getElementById("player");
+var nameEnter = document.getElementById("nameEnter");
+var highScore = document.getElementById("highScore");
+var highScorers = document.getElementById("highScorers");
+var back = document.getElementById("back");
+var highScores = document.getElementById("highScores");
+var timerEl = document.getElementById("timerEl");
+var checkAnswers = document.getElementById("checkAnswers");
 
-let start = document.querySelector("#start")
-let timerEl = document.getElementById("start");
-let questions = document.getElementById("questionsCard");
-let boxes = document.getElementById("answersBox");
-let highScore = document.getElementById("highScore"); 
-let option1 = document.getElementById("option1");
-let option2 = document.getElementById("option2");
-let option3 = document.getElementById("option3");
-let option4 = document.getElementById("option4");
-let timeInterval;
 
-let timeLeft = 90
-let currentQuestion = 0
+var timeLeft = 90;
+var secondsLeft;
+var timer;
+var scoreCount = 0;
+var currentIndex = 0;
 
+var questionText = [
+{ 
+    question: "What is Megan's real last name?",
 
-let questionsAnswers = [
-  {
-      title: "1. Megan's real last name is...",
-      choices: ["A) Pete ", "B) Jackson", "C) Thee", "D) Marino"],
-      answer: "A) Pete"
-  },
-  {
-      title: "2. She was born in...",
-      choices: ["A) New York", "B) Alabama", "C) Texas", "D) Ohio"],
-      answer: "C) Texas"
-  },
-  {
-      title: "3. Her mother was also a rapper under the name...",
-      choices: ["A) MC Lyte", "B) Lil Filly", "C) Susie P", "D) Holly Wood"],
-      answer: "D) Holly Wood"
-  },
-  {
-      title: "4. In college she studied...",
-      choices: ["A) Drama/Musical Theater", "B) Business and Finance", "C) Health Administration", "D) Hotel Management"],
-      answer: "C) Health Administration"
-  },
+    A:"Pete", 
+    B:"Jackson", 
+    C:"Thee", 
+    D:"Marino", 
+    correct: "A"
+
+},
 {
-      title: "5. Megan refers to her fans as...",
-      choices: ["A) Thee Stable", "B) Hotties", "C) Studs", "D) Meganites"],
-      answer: "B) Hotties"
-  },
-  {
-      title: "6. Megan's alter ego is..",
-      choices: ["A) Tina Snow", "B) Mercy Neaux", "C) Jovon Truth", "D) Boss Ratchet"],
-      answer: "A) Tina Snow"
-  },
+    question:"In what state was she born?",
 
+    A:"New York",
+    B:"Alabama",
+    C:"Texas",
+    D:"Ohio", 
+    correct: "C"
+},
 {
-      title: "7. Which of these is not one of Megan's biggest influences:",
-      choices: ["A) Beyonce", "B) Three 6 Mafia", "C) Pimp C", "D) Queen Latifah"],
-      answer: "D) Queen Latifah"
-  },
-  {
-      title: "8. Megan's year of birth was...",
-      choices: ["A) 1996", "B) 1997", "C) 1995", "D) 1993"],
-      answer: "C) 1995"
-  },
+    question: "Her mother was also a rapper under the name...?",
 
-]
+    A:"MC Lyte", 
+    B:"Lil Filly", 
+    C:"Susie P", 
+    D:"Hollywood", 
+    correct: "C"
+},
+{
+    question: "What did she study in college?",
 
+     A:"Drama/Musical Theater",
+     B:"Business and Finance", 
+     C:"Health Administration", 
+     D:"Hotel Management", 
+     correct: "C"
+},
+{
+    question: "Megan refers to her fans as...?",
 
+     A:"Thee Stable",
+     B:"Hotties", 
+     C:"Studs", 
+     D:"Meganites", 
+     correct: "B"
+},
+{
+    question: "Megan's alter ego is...?",
 
-/*functions*/
-function showCurrentQuestion() {
-  questions.textContent =  questionsAnswers[currentQuestion].title
-  option1.textContent = questionsAnswers[currentQuestion].choices[0]
-  option2.textContent = questionsAnswers[currentQuestion].choices[1]
-  option3.textContent = questionsAnswers[currentQuestion].choices[2]
-  option4.textContent = questionsAnswers[currentQuestion].choices[3]
-  console.log(questionsAnswers[currentQuestion].title)
+     A:"Tina Snow",
+     B:"Mercy Neaux", 
+     C:"Jovon Truth", 
+     D:"Boss Ratchet", 
+     correct: "A"
+},
+{
+    question: "Which of these is NOT one of Megan's biggest influences?",
+
+     A:"Beyonce",
+     B:"Three 6 Mafia", 
+     C:"Pimp C", 
+     D:"Queen Latifah", 
+     correct: "D"
+},
+{
+    question: "Megan was born in...?",
+
+     A:"1996",
+     B:"1997", 
+     C:"1995", 
+     D:"1993", 
+     correct: "C"
 }
 
-/*functions*/
-function checkFinished() {
-  //check to see if done
-  if (currentQuestion === 8) return true
-  return false
-}
-
-function finish() {
-  console.log("Game Over!")
-  highScore.style.display;
-  highScore.setAttribute( "style", 
-  "padding: 50px;", 
-  "border-radius: 12px;", 
-  "font-size: 75px;", 
-  "text-align: center;", 
-  "color: white;",
-  );
-  clearInterval(timeInterval);
-  
-  localStorage.setItem("Player", highScore);
-  localStorage.setItem("Hi-Score", timeLeft);
-}
-
-/*event listeners*/
-
-//when user clicks anywhere in answers box
-boxes.addEventListener("click", function (event) {
-  //check that user clicked on button
-  if (event.target.tagName === "BUTTON") {
-      //show correct / incorrect answer
-      if (event.target.textContent === questionsAnswers[currentQuestion].answer){
-      console.log("Answer: " + (event.target.textContent === questionsAnswers[currentQuestion].answer))
-      console.log("\n")
-
-      //increase question counter
-      currentQuestion++
-      }
-      else (timeLeft = timeLeft - 5)
-      console.log("Answer: " + (event.target.textContent === questionsAnswers[currentQuestion].answer))
-      console.log("\n")
-
-      //increase question counter
-      currentQuestion++
-      }
-      //go to next question or done
-      let finished = checkFinished()
-      if (finished) finish()
-      else showCurrentQuestion()
-    })
 
 
-start.addEventListener("click", function (event) {
-  questions.style.display = "block"
-  timeInterval = setInterval(function() {
-    timerEl.textContent = timeLeft + " seconds left...";
-    timeLeft--;
-    
-    if (timeLeft === 0) {
-      timerEl.textContent = "";
-      clearInterval(timeInterval);
-      finish();
-    }   
-  }, 1000);
+];
 
-  //show first question
-  showCurrentQuestion()
-})
 
-/*entry point*/
-questions.style.display = "none"
+questionArea.style.display = "none"
+scoreChart.style.display = "none"
 highScore.style.display = "none"
+
+
+function setSecondsLeft(seconds) {
+    secondsLeft = seconds;
+    timerEl.textContent = secondsLeft;
+}
+
+function startTimer() {
+    setSecondsLeft(timeLeft);
+    timer = setInterval(function() {
+        setSecondsLeft(secondsLeft-1);
+        if (secondsLeft < 0) {
+            timerEl.textContent = "0";
+            clearInterval(timer);
+            alert("Game Over");
+            finishQuiz();
+        }
+    }, 1000);
+}
+
+
+function startQuiz(){
+    promptQuestion();
+    startTimer();
+   
+}
+
+function promptQuestion(){
+    questionArea.style.display = "block";
+    start.style.display = "none";
+    var currentQ = questionText[currentIndex];
+    question.textContent = currentQ.question;
+    A.textContent = currentQ.A;
+    B.textContent = currentQ.B;
+    C.textContent = currentQ.C;
+    D.textContent = currentQ.D;
+}
+
+
+function answerOnClick(answerId) {
+    return function(event){
+        if (questionText[currentIndex].correct === answerId) {
+            scoreCount = scoreCount + 1;
+            checkAnswers.textContent = "You got that right, gurl!";
+    
+        } 
+        else {
+            setSecondsLeft(secondsLeft - 5);
+            checkAnswers.textContent = "Nuh uh, that's wrong, baby!";
+        }
+
+        currentIndex++;
+        if (currentIndex < questionText.length) {
+            promptQuestion();
+        } 
+        else {
+            finishQuiz();
+        }
+    }
+}
+
+
+function finishQuiz() {
+    clearInterval(timer);
+    timerEl.textContent = "0";
+    checkAnswers.textContent = " ";
+    showScore();
+}
+
+
+function showScore(){
+    questionArea.style.display = "none";
+    scoreChart.style.display = "block";
+    score.textContent = scoreCount;
+}
+
+
+function storeName(event){
+    event.preventDefault();
+    var nameInput = player.value
+    if (nameInput !== ""){
+        var nameScore = nameInput + " - " + scoreCount;
+        localStorage.setItem("name-score", nameScore);
+        player.value = "";
+        showHighScores();
+    } 
+    else {
+        alert("Please enter your initials")
+    }
+}
+
+
+function showHighScores(){
+    scoreChart.style.display = "none";
+    highScore.style.display = "block";
+    nameScore = localStorage.getItem("name-score");
+    var listEl = document.createElement("p");
+    listEl.textContent = nameScore;
+    highScorers.appendChild(listEl);
+
+}
+
+
+function goBackPage(){
+    questionArea.style.display = "none";
+    scoreChart.style.display = "none";
+    highScore.style.display = "none";
+    start.style.display = "block";
+    checkAnswers.textContent = " ";
+    setSecondsLeft(0);
+    scoreCount = 0;
+    currentIndex = 0;
+}
+
+
+
+function viewScores(){
+    start.style.display = "none";
+    questionArea.style.display = "none";
+    scoreChart.style.display = "none";
+    highScore.style.display = "block";
+    clearInterval(timer);
+    setSecondsLeft(0);
+}
+
+
+// event listeners 
+startButton.addEventListener("click", startQuiz);
+A.addEventListener("click", answerOnClick("A"));
+B.addEventListener("click", answerOnClick("B"));
+C.addEventListener("click", answerOnClick("C"));
+D.addEventListener("click", answerOnClick("D"));
+nameEnter.addEventListener("click", storeName);
+back.addEventListener("click", goBackPage);
+highScores.addEventListener("click", viewScores);
